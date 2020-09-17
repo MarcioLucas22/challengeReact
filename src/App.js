8dee60e5f7bbaa404a30cfc3a12d61425a67c5fa
MarcioLucas22 <48500194+MarcioLucas22@users.noreply.github.com>
2020-09-17 16:04:00 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import React from "react";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepository] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      console.log(response);
+    });
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    //setRepository([... repositories, `Novo Repositório ${Date.now()}`]);
+    const response = await api.post('repositories', {
+      title: `Novo Repositório ${Date.now()}`,
+      url: "http://github.com/..."
+    });
+
+    const repository = response.data;
+
+    setRepository([... repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+      await api.delete(`repositories/${id}`);
+
+      const newRepositories = repositories.filter(
+        repository = repository.id != id
+      ) 
+
+      setRepository(newRepositories);
   }
 
   return (
